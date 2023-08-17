1409e05fbbfc60bf6a9a20561ea40da383aa0fe7
sofia trindade <sofiaatrindadee02@gmail.com>
2023-08-17 15:14:47 -0300

    criando uma lista de objetos de musicas

---
+++ aula2_objetos/programa.js
@@ -0,0 +1,11 @@
+let musicas1 = {Titulo: 'Menina solta', Artista: 'Giulia Be', Album: 'Solta'};
+let musicas2 = {Titulo: 'Menina má', Artista: 'Anitta', Album: 'Anitta'};
+let musicas3 = {Titulo: 'Menina', Artista: 'Bia Marques', Album: 'Menina'};
+let musicas4 = {Titulo: 'Penhasco.', Artista: 'Luiza Sonza', Album: 'DOCE 22'};
+let musicas5 = {Titulo: 'Coração cigano', Artista: 'Luan Santana e Luísa Sonza', Album: 'LUAN CITY (Ao Vivo)'};
+let musicas6 = {Titulo: 'MEIO TERMO', Artista: 'Luan Santana', Album: 'LUAN CITY 2.0: FASE 1'};
+let musicas7 = {Titulo: 'Quarta Cadeira', Artista: 'Matheus & Kauan', Album: 'Tem Moda Pra Tudo (Ao Vivo)'};
+let musicas8 = {Titulo: 'Pactos', Artista: 'Matheus & Kauan', Album: 'Basiquinho'};
+let musicas9 = {Titulo: 'lovely', Artista: 'Billie Eilish e Khalid', Album: 'When We All Fall Asleep, Where Do We Go?'};
+let musicas10 = {Titulo: 'What Was I Made For?', Artista: 'Billie Eilish', Album: 'Barbie: The Album'};
+
