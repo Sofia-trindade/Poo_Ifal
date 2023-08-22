e1c48c73dc30dad935b6125665a143614a69424a
sofia trindade <sofiaatrindadee02@gmail.com>
2023-08-22 16:33:01 -0300

    adicinado as classes

---
+++ aula2_objetos/classes.js
@@ -0,0 +1,9 @@
+class Musica{
+    constructor(capa, titulo, artista, album){
+        this.capa = capa;
+        this.titulo = titulo;
+        this.artista = artista;
+        this.album = album;
+    }
+
+}
--- aula2_objetos/programa.js
+++ aula2_objetos/programa.js
@@ -1,16 +1,16 @@
 //criação dos obejtos
-let musica1 = {capa: 'menina_Solta.jpg', Titulo: 'Menina solta', Artista: 'Giulia Be', Album: 'Solta'};
-let musica2 = {capa: 'Menina_Má.png', Titulo: 'Menina má', Artista: 'Anitta', Album: 'Anitta'};
-let musica3 = {capa: 'Menina.jpg', Titulo: 'Menina', Artista: 'Bia Marques', Album: 'Menina'};
-let musica4 = {capa: 'Penhasco.jpeg', Titulo: 'Penhasco', Artista: 'Luiza Sonza', Album: 'DOCE 22'};
-let musica5 = {capa: 'Coração cigano.jpeg', Titulo: 'Coração cigano', Artista: 'Luan Santana e Luísa Sonza', Album: 'LUAN CITY (Ao Vivo)'};
-let musica6 = {capa: 'MEIO TERMO.jpeg', Titulo: 'MEIO TERMO', Artista: 'Luan Santana', Album: 'LUAN CITY 2.0: FASE 1'};
-let musica7 = {capa: 'Quarta Cadeira.jpeg', Titulo: 'Quarta Cadeira', Artista: 'Matheus & Kauan', Album: 'Tem Moda Pra Tudo (Ao Vivo)'};
-let musica8 = {capa: 'Pactos.jpeg', Titulo: 'Pactos', Artista: 'Matheus & Kauan', Album: 'Basiquinho'};
-let musica9 = {capa: 'lovely.jpeg', Titulo: 'lovely', Artista: 'Billie Eilish e Khalid', Album: 'When We All Fall Asleep, Where Do We Go?'};
-let musica10 = {capa: 'What Was.jpeg', Titulo: 'What Was I Made For?', Artista: 'Billie Eilish', Album: 'Barbie: The Album'};
+let musica1 = new Musica('menina_Solta.jpg', 'Menina solta', 'Giulia Be', 'Solta');
+let musica2 = new Musica('Menina_Má.png', 'Menina má', 'Anitta', 'Anitta');
+let musica3 = new Musica('Menina.jpg', 'Menina','Bia Marques', 'Menina');
+let musica4 = new Musica('Penhasco.jpeg', 'Penhasco', 'Luiza Sonza', 'DOCE 22');
+let musica5 = new Musica('Coração cigano.jpeg', 'Coração cigano', 'Luan Santana e Luísa Sonza', 'LUAN CITY (Ao Vivo)');
+let musica6 = new Musica('MEIO TERMO.jpeg', 'MEIO TERMO', 'Luan Santana', 'LUAN CITY 2.0: FASE 1');
+let musica7 = new Musica('Quarta Cadeira.jpeg', 'Quarta Cadeira', 'Matheus & Kauan', 'Tem Moda Pra Tudo (Ao Vivo)');
+let musica8 = new Musica('Pactos.jpeg', 'Pactos', 'Matheus & Kauan','Basiquinho');
+let musica9 = new Musica('lovely.jpeg', 'lovely', 'Billie Eilish e Khalid', 'When We All Fall Asle)p, Where Do We Go?');
+let musica10 = new Musica('What Was.jpeg', 'What Was I Made For?', 'Billie Eilish', 'Barbie: The Album');
 
-//criação do array
+//criação do arra)
 let playlistSofia = [
     musica1,
     musica2,
@@ -26,13 +26,12 @@ let playlistSofia = [
 
 // Percorrendo o array e escrevendo um div p/ cd elemento do array
 for (let i = 0; i < playlistSofia.length; i++){
-    let musica = playlistSofia[i];
     document.write(`
     <div class="card">
         <img class="capa" src="img/${playlistSofia[i].capa}"/>
-        <div class="titulo">${playlistSofia[i].Titulo}</div>
-        <div class="artista">${playlistSofia[i].Artista}</div>
-        <div class="album">${playlistSofia[i].Album}</div>
+        <div class="titulo">${playlistSofia[i].titulo}</div>
+        <div class="artista">${playlistSofia[i].artista}</div>
+        <div class="album">${playlistSofia[i].album}</div>
     </div>
     `)
 }
